af2656221934cc5311d3d81b1a6254b1a2ea5afc
DOMFAMOLSD\antonio.garcez <antonio.garcez@famo.pt>
2019-12-09 17:02:52 +0000

    [DEV] Adicionar o cordova engine

---
+++ pda/cordova/www/precache-manifest.8d9dc92ed54b2a50992b05f67814ede3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f70bc1f9a6b7050d65ed6c5350ec05dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6938dca49afa6c612e5a",
+    "url": "/static/css/main.b9d5e6b8.chunk.css"
+  },
+  {
+    "revision": "378552212954223116e4",
+    "url": "/static/js/2.20c04d2e.chunk.js"
+  },
+  {
+    "revision": "6938dca49afa6c612e5a",
+    "url": "/static/js/main.5bea3b80.chunk.js"
+  },
+  {
+    "revision": "185976c9f5e417ac3cf7",
+    "url": "/static/js/runtime-main.838a1967.js"
+  }
+]);
