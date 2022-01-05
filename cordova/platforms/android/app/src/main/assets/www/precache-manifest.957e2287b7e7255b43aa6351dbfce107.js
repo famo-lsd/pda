bf41de25059a68223b9a76efeb9bbb0fa809c751
DOMFAMOLSD\samuel.ferreira <samuel.ferreira@famo.pt>
2022-01-05 15:20:28 +0000

    .

---
+++ cordova/platforms/android/app/src/main/assets/www/precache-manifest.957e2287b7e7255b43aa6351dbfce107.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "111d67c84a49157a222bc846f97af6f1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a4f08a70ba56bc65ce45",
+    "url": "./static/css/main.c58e8e44.chunk.css"
+  },
+  {
+    "revision": "120a5124d8dc944232bf",
+    "url": "./static/js/2.82b42648.chunk.js"
+  },
+  {
+    "revision": "ef9f5173699c8a37979e67b45bfb127f",
+    "url": "./static/js/2.82b42648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4f08a70ba56bc65ce45",
+    "url": "./static/js/main.99cc2ca7.chunk.js"
+  },
+  {
+    "revision": "6bb30dbb3560f9babcad",
+    "url": "./static/js/runtime-main.80326ce4.js"
+  }
+]);
