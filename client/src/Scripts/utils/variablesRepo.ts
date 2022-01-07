0a73f4dcc3017538c4224850cfa2d7b3c7b3ddc8
António Garcez <antonio.garcez@famo.pt>
2022-01-07 16:47:46 +0000

    [PATCH] Correção ao último patch.

--- client/src/Scripts/utils/variablesRepo.ts
+++ client/src/Scripts/utils/variablesRepo.ts
@@ -1,4 +1,4 @@
 export const LOG_APP_NAME = 'pda',
-    NODE_SERVER = process.env.REACT_APP_NODE_SERVER,
+    NODE_SERVER = process.env.REACT_APP_NODE_SERVER.replace('localhost', window.location.hostname),
     NODE_TOKEN_PREFIX = 'noken',
     NODE_TOKEN_KEY = 'access_' + NODE_TOKEN_PREFIX;
