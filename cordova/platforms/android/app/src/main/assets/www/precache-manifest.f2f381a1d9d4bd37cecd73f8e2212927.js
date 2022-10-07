142bac93e90ea8dd8df2eadc5cae2546c6e4e06b
DOMFAMOLSD\samuel.ferreira <samuel.ferreira@famo.pt>
2022-10-07 10:25:28 +0100

    [DEV] .

---
+++ cordova/platforms/android/app/src/main/assets/www/precache-manifest.f2f381a1d9d4bd37cecd73f8e2212927.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03163114f488a6506d558bc71d0dc661",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eb5ec969593f565ad6c9",
+    "url": "./static/css/main.96b294b4.chunk.css"
+  },
+  {
+    "revision": "47a9cbd5648f9987cd50",
+    "url": "./static/js/2.1256f458.chunk.js"
+  },
+  {
+    "revision": "ef9f5173699c8a37979e67b45bfb127f",
+    "url": "./static/js/2.1256f458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb5ec969593f565ad6c9",
+    "url": "./static/js/main.2054180d.chunk.js"
+  },
+  {
+    "revision": "6bb30dbb3560f9babcad",
+    "url": "./static/js/runtime-main.80326ce4.js"
+  }
+]);
--- cordova/platforms/android/app/src/main/assets/www/service-worker.js
+++ cordova/platforms/android/app/src/main/assets/www/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d73e79133f4134b65a03485d6399a83a.js"
+  "./precache-manifest.f2f381a1d9d4bd37cecd73f8e2212927.js"
 );
 
 self.addEventListener('message', (event) => {
